863b55333849520db49b1cedbf31ad98c5c91e90
erebb <77569663+erebb@users.noreply.github.com>
2023-01-24 23:53:27 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Azoth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.metroPanel1.Controls.Add(this.button4);
+            this.metroPanel1.Controls.Add(this.button3);
+            this.metroPanel1.Controls.Add(this.label3);
+            this.metroPanel1.Controls.Add(this.label1);
+            this.metroPanel1.Controls.Add(this.textBox1);
+            this.metroPanel1.Controls.Add(this.button2);
+            this.metroPanel1.Controls.Add(this.button1);
+            this.metroPanel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(-3, -2);
+            this.metroPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(262, 341);
+            this.metroPanel1.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroPanel1.TabIndex = 1;
+            this.metroPanel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 13;
+            this.metroPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.metroPanel1_Paint);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button4.Enabled = false;
+            this.button4.ForeColor = System.Drawing.Color.Coral;
+            this.button4.Location = new System.Drawing.Point(212, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(29, 26);
+            this.button4.TabIndex = 25;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(212, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 30);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Gothic", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(142, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 19);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Status:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "WizBOT";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 293);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MS Gothic", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Navy;
+            this.button2.Location = new System.Drawing.Point(0, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "HWID CHECK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MS Gothic", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Navy;
+            this.button1.Location = new System.Drawing.Point(3, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 85);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 322);
+            this.Controls.Add(this.metroPanel1);
+            this.Font = new System.Drawing.Font("MS Gothic", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Wizard 101 Script";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace Azoth
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        public int v = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // STATUS OTO GÜNCELLEME CROOSHAİR EKLE
+        private void metroPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            string hedef = "https://github.com/erebb/hwidtxt3/blob/main/gncl.txt"; //Buraya güncelleme sürümünü kontrol edecek adresi giriyorsunuz.
+            WebRequest istek = HttpWebRequest.Create(hedef);
+            WebResponse yanit;
+            yanit = istek.GetResponse();
+            StreamReader bilgiler = new StreamReader(yanit.GetResponseStream());
+            string gelen = bilgiler.ReadToEnd();
+            int baslangic = gelen.IndexOf("<p>") + 3;
+            int bitis = gelen.Substring(baslangic).IndexOf("</p>");
+            string gelenbilgileri = gelen.Substring(baslangic, bitis);
+            v = Convert.ToInt16(gelenbilgileri);
+            timer1.Start();
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        Process p = Process.GetProcessesByName("Eren").FirstOrDefault();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var mbs = new ManagementObjectSearcher("Select ProcessorId From Win32_processor");
+            ManagementObjectCollection mbsList = mbs.Get();
+            string id = "";
+            foreach (ManagementObject maObject in mbsList)
+            {
+                id = maObject["ProcessorId"].ToString();
+                break;
+            }
+            string da12 = "1";// her versiyon geldiğinde bunu artır güncelleme getirmek istedğiğnde sitedeki dosyayıda  de bir artır
+            WebClient webc12 = new WebClient();
+            var gncl1 = webc12.DownloadString("https://github.com/erebb/hwidtxt3/blob/main/gncl.txt");
+            WebClient webc = new WebClient();
+            var idler = webc.DownloadString("https://github.com/erebb/hwidtxt3/blob/main/hwid.txt");
+                if (idler.Contains(id)) // Eğer sitemizdeki ID'lerde kullanıcının CPU ID'si varsa yapılacak işlem.
+            {
+                if (gncl1.Contains(da12))
+                {
+                    MessageBox.Show("Updated", ":)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("New Version.", ":(", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    System.Diagnostics.Process.Start("https://github.com/erebb/hwidtxt3/raw/main/WizBot.exe");
+                    Environment.Exit(0);
+                }
+
+
+
+                // Ekrana giriş başarılı yazdırıyoruz.
+                WebClient client31 = new WebClient();
+               
+                MessageBox.Show("Please Wait Loading Script.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                WebClient client69 = new WebClient();
+                client69.DownloadFile("https://s3.eu-central-1.amazonaws.com/blob-eu-central-1-nwsekq/sara/25/2517/25175f41-de68-46a0-a271-50f78727f7d6.bin?response-content-disposition=attachment%3B%20filename%3D%22WizBot.exe%22&response-content-type=application%2Fx-msdownload&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4DM2EY46SOWUMUP4%2F20230112%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230112T150407Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1800&X-Amz-Signature=10dea0d56b6a28ad3609e205b46a0fef767be3acb3c851be1a13a4ae031c3254", "C:/Windows/Temp/WizBot.exe");
+                Thread.Sleep(900);
+                MessageBox.Show("Welcome.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Process process = new Process();
+                
+                process.StartInfo.FileName = @"C:/Windows/Temp/WizBot.exe";
+               
+                process.Start();
+                Thread.Sleep(350);
+                Process p = Process.GetProcessesByName("WizBot").FirstOrDefault();
+                if (p != null)
+                {
+                    IntPtr h = p.MainWindowHandle;
+                    SetForegroundWindow(h);
+                    SendKeys.SendWait("316962135783928343201984213423412344324");
+                    Thread.Sleep(1);
+                    SendKeys.SendWait("{Enter}");
+                }
+
+
+
+            }
+            else // Eğer yoksa yapılacak işlem.
+            {
+                // Ekrana giriş yapılamadı yazdırıyoruz.
+                MessageBox.Show("Unregistered HWID", "!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Random rand = new Random();
+            int one = rand.Next(0, 255);
+            int two = rand.Next(0, 255);
+            int three = rand.Next(0, 255);
+            int four = rand.Next(0, 255);
+            button1.BackColor = Color.FromArgb(one, two, three, four);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var mbs = new ManagementObjectSearcher("Select ProcessorId From Win32_processor");
+            ManagementObjectCollection mbsList = mbs.Get();
+            string id = "";
+            foreach (ManagementObject maObject in mbsList)
+            {
+                id = maObject["ProcessorId"].ToString();
+                break;
+            }
+
+            textBox1.Text = id; // TextBox'a CPU ID'yi yazdırıyoruz.
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string b = "1";// her versiyon geldiğinde bunu artır güncelleme getirmek istedğiğnde siteden de bir artır
+            WebClient webc1 = new WebClient();
+            var gncl = webc1.DownloadString("https://hwid31.000webhostapp.com/gncl.txt");
+            if (gncl.Contains(b)) // Eğer sitemizdeki ID'lerde kullanıcının CPU ID'si varsa yapılacak işlem.
+            {
+                MessageBox.Show("Updated.", "!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Güncelleme Varsa Güncelleyin Yoksa Ban Yersiniz
+            }
+            else // Eğer yoksa yapılacak işlem.
+            {
+                // Ekrana giriş başarılı yazdırıyoruz.
+                MessageBox.Show("New Version.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                System.Diagnostics.Process.Start("https://hwid31.000webhostapp.com/WizBot.exe");
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        // JUNK CODE GÜNCELLECEĞİNDE DEĞİŞTİR
+
+
+    }
+    }
